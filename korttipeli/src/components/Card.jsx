9a1713e6c1491aec633a405e1883ad0380ae81c3
villejokinen <villeejjokinen@gmail.com>
2023-09-07 19:26:26 +0300

    Added the first card

---
+++ korttipeli/src/components/Card.jsx
@@ -0,0 +1,11 @@
+
+export default function Card(){
+    return(
+        <div>
+            <img src="https://placekitten.com/120/100"/>
+            <span>Looks</span>
+            <span>Health</span>
+        </div>
+        
+    );
+}
