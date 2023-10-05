1355d030cdc0480c0845fd22a9c13ee985176920
villejokinen <villeejjokinen@gmail.com>
2023-10-05 19:28:53 +0300

    worked on the deck

--- korttipeli/src/App.jsx
+++ korttipeli/src/App.jsx
@@ -4,15 +4,7 @@ import {useState} from 'react'
 
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-const createCards = index => ({
-    image: `http://placekitten.com/140/120?image=${index}`,
-    stats: [
-    { name: 'Looks', value: getRandomInt(1, 999) }, 
-    { name: 'Speed', value: getRandomInt(1, 999) },
-    {name: 'Weight', value: getRandomInt(1, 999) },
-  ],
-    id: crypto.randomUUID()
-})
+
 
 const playerCard = {
   image: "https://placekitten.com/120/100",
@@ -30,19 +22,35 @@ const opponentCard = {
           {name: 'Speed', value: 12}]
 }
 
+const createCard = index => ({
+    image: `http://placekitten.com/140/120?image=${index}`,
+    stats: [
+    { name: 'Looks', value: getRandomInt(1, 999) }, 
+    { name: 'Speed', value: getRandomInt(1, 999) },
+    {name: 'Weight', value: getRandomInt(1, 999) },
+  ],
+    id: crypto.randomUUID()
+})
 
-
-
+const deck = Array(16).fill(null).map((_,index)=>createCard(index))
+const half = Math.ceil(deck.length / 2)
+const dealCards = () => {
+  return {
+    player: deck.slice(0, half),
+    opponent: deck.slice(half)
+  }
+}
 
 export default function App() {
   
   const[result, setResult] = useState('')
-
+  const[cards, setCards] = useState(dealCards)
+  
   function compareCards() {
 
 
-    const playerStats = playerCard.stats[0];
-    const opponentStats = opponentCard.stats[0];
+    const playerStats = cards.player[0].stats[0];
+    const opponentStats = cards.opponent[0].stats[0];
 
     if (playerStats.value > opponentStats.value) {
       setResult('win')
@@ -59,13 +67,20 @@ export default function App() {
     <div>
       <h1>Card Game</h1>
       <div className='game'>
-        <Card card={playerCard} />
+        <ul className='card-list'>
+          {cards.player.map(playerC => (
+            <li>
+              <Card card={playerC} />
+
+            </li>
+          ))}
+        </ul>
         <div className='center-area'>
           <p>{result || 'Press the button'}</p>
           <button type='button' onClick={compareCards}>Play</button>
         </div>
-        <Card card={opponentCard} />
-        
+        <Card card={cards.opponent[0]} />
+        {console.log(dealCards())}
       </div>
       
     </div>
